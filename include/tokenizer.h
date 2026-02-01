9af5d0adda94270bc014115ddd343d482158f6b2
George-Markas <georgemarkanton@proton.me>
2026-02-02 01:03:53 +0200

    Added comments

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -29,6 +29,17 @@ typedef struct Token {
 
 extern const char *tok_type_to_str[];
 
+/**
+ * @brief Load the contents of a text file into a string.
+ * @param filename The path to the file.
+ * @return A pointer to the allocated string buffer.
+ * @note The string buffer can be freed with free().
+ */
 char *file_to_string(const char *filename);
 
+/**
+ * @brief Breaks up the provided string into JSON tokens.
+ * @param str The string of the text to be tokenized.
+ * @return an array list containing the tokens.
+ */
 AList_t *tokenizer(const char *str);
