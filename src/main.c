003225d78694d7127e3e06f8c19e4e0a3c5dc929
George-Markas <georgemarkanton@proton.me>
2026-02-08 16:20:54 +0200

    Added proper handling for booleans, numbers and null

--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 #include "parser.h"
 
 #ifndef NDEBUG
-#define TEST_TEXT "../tests/text.txt"
 #define TEST_JSON "../tests/simple_json.txt"
 #define TEST_U8_JSON "../tests/utf-8_json.txt"
+#define TEST_JSON_TYPES "../tests/json_types.txt"
 #endif
 
 int main(void) {
     register_uchar8_t_printf();
 
-    const char *str = file_to_string(TEST_U8_JSON);
+    const char *str = file_to_string(TEST_JSON_TYPES);
     parse(str);
 
     free((void *) str);
