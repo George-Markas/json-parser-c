c24926b2583f4f8d75de24032e4f4a059d7ca1cf
George-Markas <georgemarkanton@proton.me>
2026-02-08 19:19:11 +0200

    Removed path macros because of platform weirdness

--- src/main.c
+++ src/main.c
@@ -3,16 +3,10 @@
 #include "tokenizer.h"
 #include "parser.h"
 
-#ifndef NDEBUG
-#define TEST_JSON "../tests/simple_json.txt"
-#define TEST_U8_JSON "../tests/utf-8_json.txt"
-#define TEST_JSON_TYPES "../tests/json_types.txt"
-#endif
-
 int main(void) {
     register_uchar8_t_printf();
 
-    const char *str = file_to_string(TEST_JSON_TYPES);
+    const char *str = file_to_string("../tests/json_types.txt");
     parse(str);
 
     free((void *) str);
